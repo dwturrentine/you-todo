c52b23ca9f7f731cb6a04a3649cf3c8e43c7b8f0
Daryl <inbox.dwt@gmail.com>
2020-12-27 12:23:41 -0500

    Add files via upload

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import { List } from "@material-ui/core";
+import React from "react";
+import Todo from "./Todo";
+
+function TodoList( {todos, toggleComplete, removeTodo} ){
+    return(
+        <List>
+            {todos.map(todo => (
+                <Todo 
+                key={todo} 
+                todo={todo} 
+                toggleComplete={toggleComplete}
+                removeTodo={removeTodo}
+                />
+            ))}
+        </List>
+
+    );
+
+}
+
+export default TodoList;
